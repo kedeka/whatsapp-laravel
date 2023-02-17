ccf4061b89b184729932be1893e3afd655f832ee
riskihajar <riskihajar@gmail.com>
2023-02-17 09:23:40 +0800

    fetch profile picture

---
+++ src/ProfilePicture.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kedeka\Whatsapp;
+
+class ProfilePicture extends Client implements Contracts\OnWhatsApps
+{
+    public function fetch($phone)
+    {
+        $phone = preg_replace("/[^0-9]/", "", $phone);
+
+        $response = $this->request('/profile-picture', [
+            'phone' => $phone,
+        ]);
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return $data['url'] ?? null;
+    }
+}

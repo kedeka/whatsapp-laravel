2860563cea19ee96b86e715dd1c0b909781bc10f
riskihajar <riskihajar@gmail.com>
2023-02-17 09:43:38 +0800

    add profile name

---
+++ src/ProfileName.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kedeka\Whatsapp;
+
+class ProfileName extends Client implements Contracts\OnWhatsApps
+{
+    public function fetch($phone)
+    {
+        $phone = preg_replace("/[^0-9]/", "", $phone);
+
+        $response = $this->request('/profile', [
+            'phone' => $phone,
+        ]);
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return $data['name'] ?? null;
+    }
+}
